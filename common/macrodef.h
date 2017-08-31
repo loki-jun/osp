f5cc17aa809eb5921625ed90568fa41e13ae0e9f
loki-jun <ustcljun@gmail.com>
2017-08-31 16:47:50 +0800

    分级打印

--- common/macrodef.h
+++ common/macrodef.h
@@ -25,4 +25,16 @@
 
 #define SERVER_APP_NO               1
 
+// log等级定义
+#define	LOG_LVL_ERROR				(u8)1				// 程序运行错误(逻辑或业务出错),必须输出
+#define	LOG_LVL_WARNING				(u8)2				// 告警信息, 可能正确, 也可能错误
+#define	LOG_LVL_KEYSTATUS			(u8)3				// 程序运行到一个关键状态时的信息输出
+#define	LOG_LVL_DETAIL				(u8)4				// 普通信息, 最好不要写进log文件中
+#define	LOG_LVL_REPEAT				(u8)5				// 更高级别打印
+
+//设置log打印级别
+#define	APP_ID                      0                   //全局
+#define	FILE_LEVEL                  0                   //禁止输出到文件
+#define	SCREEN_LEVEL                5                   //屏幕打印阈值
+
 #endif
