ec609d9718576cc51166452d8489887b774a8f87
loki-jun <ustcljun@gmail.com>
2017-09-06 09:13:41 +0800

    加消息体内容说明

--- client/include/clientinterface.h
+++ client/include/clientinterface.h
@@ -13,15 +13,15 @@
 #ifndef _CLIENTINTERFACE_H_
 #define _CLIENTINTERFACE_H_
 
-#define U_C_CONNECT_CMD            (u16)(OSP_USEREVENT_BASE+0x0013)
-#define U_C_DISCONNECT_CMD         (u16)(OSP_USEREVENT_BASE+0x0016)
-#define U_C_GETLIST_CMD            (u16)(OSP_USEREVENT_BASE+0x0017)
-#define U_C_DOWNLOADFILE_CMD       (u16)(OSP_USEREVENT_BASE+0x0018)
-#define U_C_PAUSETASK_CMD          (u16)(OSP_USEREVENT_BASE+0x0019)
-#define U_C_RESUMETASK_CMD         (u16)(OSP_USEREVENT_BASE+0x001A)
-#define U_C_CANCELTASK_CMD         (u16)(OSP_USEREVENT_BASE+0x001B)
-#define U_C_DOWNLOADPARTFILE_CMD   (u16)(OSP_USEREVENT_BASE+0x001C)
-#define U_C_CANCELPARTFILE_CMD     (u16)(OSP_USEREVENT_BASE+0x001D)
+#define U_C_CONNECT_CMD            (u16)(OSP_USEREVENT_BASE+0x0013) //连接命令，user->client(daemon)，消息体为u32 Ip
+#define U_C_DISCONNECT_CMD         (u16)(OSP_USEREVENT_BASE+0x0016) //断开连接，user->client(daemon)
+#define U_C_GETLIST_CMD            (u16)(OSP_USEREVENT_BASE+0x0017) //获取文件列表命令，user->client(daemon)
+#define U_C_DOWNLOADFILE_CMD       (u16)(OSP_USEREVENT_BASE+0x0018) //下载文件命令，user->client(daemon)，消息体为s8(pbyFilename)
+#define U_C_PAUSETASK_CMD          (u16)(OSP_USEREVENT_BASE+0x0019) //暂停下载，user->client(daemon)，消息体为u16 taskID
+#define U_C_RESUMETASK_CMD         (u16)(OSP_USEREVENT_BASE+0x001A) //恢复下载，user->client(daemon)，消息体为u16 taskID
+#define U_C_CANCELTASK_CMD         (u16)(OSP_USEREVENT_BASE+0x001B) //取消下载，user->client(daemon)，消息体为u16 taskID
+#define U_C_DOWNLOADPARTFILE_CMD   (u16)(OSP_USEREVENT_BASE+0x001C) //下载断点续传文件，user->client(daemon)，消息体为CConfigData
+#define U_C_CANCELPARTFILE_CMD     (u16)(OSP_USEREVENT_BASE+0x001D) //取消断点续传文件下载，user->client(daemon)，消息体为s8(pbyFilename)
 
 void clientinterface(void);
 void showlist(CMessage *const pMsg);
--- common/csmsg.h
+++ common/csmsg.h
@@ -1,7 +1,7 @@
 /*==========================================================                       
 文件名：csmsg.h
 相关文件：
-实现功能：定义客户端与服务器之间的消息
+实现功能：定义客户端与服务器之间的消息以及公共类定义
 作者：
 版权：
 ------------------------------------------------------------
@@ -14,33 +14,33 @@
 #define _CSMSG_H_
 
 /* 连接服务器 */
-#define C_S_CONNECT_REQ              (u16)(OSP_USEREVENT_BASE+0x0001)
-#define S_C_CONNECT_ACK              (u16)(OSP_USEREVENT_BASE+0x0002)
-#define S_C_CONNECT_NACK             (u16)(OSP_USEREVENT_BASE+0x0003)
+#define C_S_CONNECT_REQ              (u16)(OSP_USEREVENT_BASE+0x0001) //客户端连接请求，client(daemon)->server(daemon)
+#define S_C_CONNECT_ACK              (u16)(OSP_USEREVENT_BASE+0x0002) //服务器响应连接请求，server(daemon)->client(daemon)
+#define S_C_CONNECT_NACK             (u16)(OSP_USEREVENT_BASE+0x0003) //服务器拒接客户端连接请求，server(daemon)->client(daemon)
 
 /* 请求注册 */
-#define C_S_REGISTER_REQ             (u16)(OSP_USEREVENT_BASE+0x0004)
-#define S_C_REGISTER_ACK             (u16)(OSP_USEREVENT_BASE+0x0005)
-#define S_C_REGISTER_NACK            (u16)(OSP_USEREVENT_BASE+0x0006)
+#define C_S_REGISTER_REQ             (u16)(OSP_USEREVENT_BASE+0x0004) //客户端instance注册请求，client(instance)->server(instance)
+#define S_C_REGISTER_ACK             (u16)(OSP_USEREVENT_BASE+0x0005) //服务器响应客户端注册请求，server(instance)->client(instance)
+#define S_C_REGISTER_NACK            (u16)(OSP_USEREVENT_BASE+0x0006) //服务器拒绝客户端注册请求，server(instance)->client(instance)
 
 /* 获取文件列表 */
-#define C_S_GETLIST_REQ              (u16)(OSP_USEREVENT_BASE+0x0007)
-#define S_C_GETLIST_ACK              (u16)(OSP_USEREVENT_BASE+0x0008)
-#define S_C_GETLIST_NACK             (u16)(OSP_USEREVENT_BASE+0x0009)
+#define C_S_GETLIST_REQ              (u16)(OSP_USEREVENT_BASE+0x0007) //客户端获取文件列表请求，client(daemon)->server(daemon)
+#define S_C_GETLIST_ACK              (u16)(OSP_USEREVENT_BASE+0x0008) //服务器响应客户端文件列表请求，server(daemon)->client(daemon)，消息体为CFileListInfo
+#define S_C_GETLIST_NACK             (u16)(OSP_USEREVENT_BASE+0x0009) //服务器拒绝客户端获取文件列表，server(daemon)->client(daemon)
 
 /* 文件下载 */
-#define C_S_FILENAME_REQ             (u16)(OSP_USEREVENT_BASE+0x000A) /*下载文件请求*/
-#define S_C_FILENAME_ACK             (u16)(OSP_USEREVENT_BASE+0x000B) /*文件存在确认*/
-#define S_C_FILENAME_NACK            (u16)(OSP_USEREVENT_BASE+0x000C)
+#define C_S_FILENAME_REQ             (u16)(OSP_USEREVENT_BASE+0x000A) //客户端文件下载请求，client(instance)->server(instance)，消息体为s8(pbyFilename[256])
+#define S_C_FILENAME_ACK             (u16)(OSP_USEREVENT_BASE+0x000B) //服务器响应客户端文件下载请求，server(instance)->client(instance)，消息体为CFileInfo
+#define S_C_FILENAME_NACK            (u16)(OSP_USEREVENT_BASE+0x000C) //服务器拒绝客户端文件下载请求，server(instance)->client(instance)
 
-#define C_S_DOWNLOADDATA_REQ         (u16)(OSP_USEREVENT_BASE+0x000D) /*数据包下载请求*/
-#define S_C_DOWNLOADDATA_ACK         (u16)(OSP_USEREVENT_BASE+0x000E) /*返回数据包的数据*/
-#define S_C_DOWNLOADDATA_NACK        (u16)(OSP_USEREVENT_BASE+0x000F)
+#define C_S_DOWNLOADDATA_REQ         (u16)(OSP_USEREVENT_BASE+0x000D) //客户端数据包下载请求，client(instance)->server(instance)，消息体为CPackageInfo
+#define S_C_DOWNLOADDATA_ACK         (u16)(OSP_USEREVENT_BASE+0x000E) //服务器返回数据包的数据给客户端，server(instance)->client(instance)，消息体为CPackageInfo
+#define S_C_DOWNLOADDATA_NACK        (u16)(OSP_USEREVENT_BASE+0x000F) //服务器拒绝数据包传输，server(instance)->client(instance)
 
 /* 取消文件下载 */
-#define C_S_CANCELFILE_REQ           (u16)(OSP_USEREVENT_BASE+0x0010)
-#define S_C_CANCELFILE_ACK           (u16)(OSP_USEREVENT_BASE+0x0011)
-#define S_C_CANCELFILE_NACK          (u16)(OSP_USEREVENT_BASE+0x0012)
+#define C_S_CANCELFILE_REQ           (u16)(OSP_USEREVENT_BASE+0x0010) //客户端取消文件下载请求，client(instance)->server(instance)，消息体为s8(pbyFilename[256])
+#define S_C_CANCELFILE_ACK           (u16)(OSP_USEREVENT_BASE+0x0011) //服务器响应客户端取消文件下载，server(instance)->client(instance)
+#define S_C_CANCELFILE_NACK          (u16)(OSP_USEREVENT_BASE+0x0012) //服务器拒绝取消下载请求，server(instance)->client(instance)
 
 class CFileInfo
 {
